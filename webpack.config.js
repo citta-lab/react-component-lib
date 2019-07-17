2d6078a34e3ac986aa1082eb9164b8f53b66767c
citta-lab <mahesh2684@gmail.com>
2019-07-17 17:10:09 -0400

    use babelrc in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    path: path.resolve(__dirname, './lib'),
-    filename: 'index.js',
-    library: '',
-    libraryTarget: 'commonjs',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: [
-            'array-includes',
-            '@babel/plugin-transform-object-assign',
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/plugin-transform-runtime',
-            '@babel/plugin-syntax-dynamic-import',
-            '@babel/plugin-proposal-class-properties',
-          ],
-        },
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, './lib'),
+        filename: 'index.js',
+        library: '',
+        libraryTarget: 'commonjs',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
